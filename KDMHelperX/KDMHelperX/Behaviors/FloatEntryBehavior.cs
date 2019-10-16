c73021aa4075c5487b55a117129f3c17f4c7a8fe
Julius <julius.narvilas@gmail.com>
2019-10-16 23:32:29 +0100

    Adding WIP numerical entry behaviors

---
+++ KDMHelperX/KDMHelperX/Behaviors/FloatEntryBehavior.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace KDMHelperX.Behaviors
+{
+    public class FloatEntryBehavior : Behavior<Entry>
+    {
+        public static readonly BindableProperty IsValidProperty = BindableProperty.Create("IsValid", typeof(bool), typeof(IntEntryBehavior), false);
+        public static readonly BindableProperty IsRequiredProperty = BindableProperty.Create("IsRequired", typeof(bool), typeof(IntEntryBehavior), false);
+        public static readonly BindableProperty MinValueProperty = BindableProperty.Create("MinValue", typeof(long), typeof(IntEntryBehavior), (long)int.MinValue);
+        public static readonly BindableProperty MaxValueProperty = BindableProperty.Create("MaxValue", typeof(long), typeof(IntEntryBehavior), (long)int.MaxValue);
+
+        private bool m_insideChangeCallback = true;
+
+        public long MinValue
+        {
+            get { return (long)GetValue(MinValueProperty); }
+            set { SetValue(MinValueProperty, value); }
+        }
+        public long MaxValue
+        {
+            get { return (long)GetValue(MaxValueProperty); }
+            set { SetValue(MaxValueProperty, value); }
+        }
+
+        public bool IsValid
+        {
+            get { return (bool)GetValue(IsValidProperty); }
+            set { SetValue(IsValidProperty, value); }
+        }
+        public bool IsRequired
+        {
+            get { return (bool)GetValue(IsValidProperty); }
+            set { SetValue(IsValidProperty, value); }
+        }
+
+        private bool TestIfConditionsAreValid(string value)
+        {
+            bool valueIsRequired = IsRequired;
+            if (string.IsNullOrEmpty(value))
+            {
+                return !valueIsRequired;
+            }
+            else
+            {
+                if (value[0] == '-')
+                {
+                    //"-" is not a valid number even for non required values
+                    if(value.Length == 1)
+                        return false;
+                }
+
+                double parsedValue;
+                if (double.TryParse(value, out parsedValue))
+                {
+                    return parsedValue >= MinValue && parsedValue <= MaxValue;
+                }
+            }
+            return false;
+        }
+
+        private bool TestIfNumberIsValid(string value)
+        {
+            bool hadDot = false;
+            bool wasValid = true;
+            int valueSize = value.Length;
+
+            for (int valueStrIndex = 0; valueStrIndex < valueSize; valueStrIndex++)
+            {
+                char currentChar = value[valueStrIndex];
+                if (!char.IsDigit(currentChar))
+                {
+                    if (currentChar == '-')
+                    {
+                        if (valueStrIndex == 0 && value.Length > 1)
+                        {
+                            continue;
+                        }
+                    }
+                    else if (!hadDot && currentChar == '.')
+                    {
+                        if(!hadDot && valueStrIndex > 0 && char.IsDigit(value[valueStrIndex-1]))
+                        {
+                            hadDot = true;
+                            continue;
+                        }
+                    }
+                    wasValid = false;
+                    break;
+                }
+            }
+
+            return wasValid;
+        }
+
+        private bool TestIfValid(string value)
+        {
+            bool finalIsValid = true;
+            if (!string.IsNullOrEmpty(value))
+            {
+                finalIsValid = TestIfNumberIsValid(value);
+            }
+            if (finalIsValid)
+            {
+                finalIsValid = TestIfConditionsAreValid(value);
+            }
+
+            return finalIsValid;
+        }
+
+
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.Keyboard = Keyboard.Numeric;
+            IsValid = TestIfValid(bindable.Text);
+            bindable.TextChanged += bindable_TextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= bindable_TextChanged;
+        }
+
+        private void bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (m_insideChangeCallback)
+                return;
+
+            m_insideChangeCallback = true;
+
+            if (TestIfValid(e.NewTextValue))
+            {
+                IsValid = true;
+            }
+            else
+            {
+                //return old value
+                ((Entry)sender).Text = e.OldTextValue;
+            }
+
+            m_insideChangeCallback = false;
+        }
+    }
+}
---
+++ KDMHelperX/KDMHelperX/Behaviors/IntEntryBehavior.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace KDMHelperX.Behaviors
+{
+
+    public class IntEntryBehavior : Behavior<Entry>
+    {
+        public static readonly BindableProperty IsValidProperty = BindableProperty.Create("IsValid", typeof(bool), typeof(IntEntryBehavior), false);
+        public static readonly BindableProperty MinDigitCountProperty = BindableProperty.Create("MinDigitCount", typeof(int), typeof(IntEntryBehavior), 0);
+        public static readonly BindableProperty MaxDigitCountProperty = BindableProperty.Create("MaxDigitCount", typeof(int), typeof(IntEntryBehavior), 0);
+        public static readonly BindableProperty MinValueProperty = BindableProperty.Create("MinValue", typeof(long), typeof(IntEntryBehavior), (long)int.MinValue);
+        public static readonly BindableProperty MaxValueProperty = BindableProperty.Create("MaxValue", typeof(long), typeof(IntEntryBehavior), (long)int.MaxValue);
+
+        public int MinDigitCount
+        {
+            get { return (int)GetValue(MinDigitCountProperty); }
+            set { SetValue(MinDigitCountProperty, value); }
+        }
+        public int MaxDigitCount
+        {
+            get { return (int)GetValue(MaxDigitCountProperty); }
+            set { SetValue(MaxDigitCountProperty, value); }
+        }
+        public long MinValue
+        {
+            get { return (long)GetValue(MinValueProperty); }
+            set { SetValue(MinValueProperty, value); }
+        }
+        public long MaxValue
+        {
+            get { return (long)GetValue(MaxValueProperty); }
+            set { SetValue(MaxValueProperty, value); }
+        }
+
+        public bool IsValid
+        {
+            get { return (bool)GetValue(IsValidProperty); }
+            set { SetValue(IsValidProperty, value); }
+        }
+
+        private bool TestIfValid(string value)
+        {
+            int minDigits = MinDigitCount;
+            if (string.IsNullOrEmpty(value))
+            {
+                return minDigits == 0;
+            }
+            else
+            {
+                int valDigits = value.Length;
+                if (value[0] == '-')
+                {
+                    //"-" is not a valid number even for 0 min digit count
+                    if (value.Length == 1)
+                        return false;
+
+                    --valDigits;
+                }
+
+                int maxDigits = MaxDigitCount;
+                if(valDigits >= minDigits && (maxDigits == 0 || valDigits <= maxDigits))
+                {
+                    long parsedValue;
+                    if(long.TryParse(value, out parsedValue))
+                    {
+                        return parsedValue >= MinValue && parsedValue <= MaxValue;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.Keyboard = Keyboard.Numeric;
+            IsValid = TestIfValid(bindable.Text);
+            bindable.TextChanged += bindable_TextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= bindable_TextChanged;
+        }
+
+        private void bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            IsValid = TestIfValid(e.NewTextValue);
+        }
+    }
+}
